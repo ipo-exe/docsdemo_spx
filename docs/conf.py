e9897dde81b3f5c70aa7db4b038da5d9fabb0e3c
Iporã Possantti <61351163+ipo-exe@users.noreply.github.com>
2022-10-27 15:07:43 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
+project = "Basic Sphinx Demo Project"
 copyright = "2022, Read the Docs core team"
 author = "Read the Docs core team"
 
